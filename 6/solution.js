870d8dae19e0365df4012d6989564969ffb71b90
James Spencer <jmes.spncer@gmail.com>
2020-12-11 22:24:50 +0000

    feat: problem six

---
+++ 6/solution.js
@@ -0,0 +1,13 @@
+// --- Problem Six ---
+
+// Find the difference between the sum of the squares of the first one hundred
+// natural numbers and the square of the sum.
+
+const sumArrayValues = require('../lib/sumArrayValues')
+
+const nums = Array.from({ length: 100 }, (_, k)=> k + 1)
+const sumOfSquares = sumArrayValues(nums.map(num => Math.pow(num, 2)))
+const squareOfSum = Math.pow(sumArrayValues(nums), 2)
+const difference = squareOfSum - sumOfSquares
+
+console.log(difference)
